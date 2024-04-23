25a996bb0ff87d465e5d4e0b59dc69dcf9a07c77
sidpani9001 <sidharthpani@gmail.com>
2024-04-23 12:16:21 +0530

    adding div function

--- data_processing.py
+++ data_processing.py
@@ -1,1 +1,21 @@
 print("add all the files for data processing")
+
+a=5
+b=7
+
+def sum(a,b):
+    c=a+b
+    return c
+
+print("sum function")
+
+def mul(a,b):
+    c=a*b
+    return c
+
+print("multiple function")
+
+def div(a,b):
+    c=a/b
+    return c
+print("add div function")
