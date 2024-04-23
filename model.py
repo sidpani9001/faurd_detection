75470e9d2ea0dbb06a243f0801d593f6cb35c1dd
sidpani9001 <sidharthpani@gmail.com>
2024-04-23 12:34:46 +0530

    adding models

--- model.py
+++ model.py
@@ -1,1 +1,10 @@
 print("adding all machine learning models")
+
+def model1():
+    print("adding model 1")
+    
+def model2():
+    print("adding model 1")
+
+def model3():
+    print("adding model 1")
